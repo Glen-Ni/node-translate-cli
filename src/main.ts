5c39000890224f4e6babbfb362fc8d5aea1092d5
小僵尸RUA <nijialun888@hotmail.com>
2022-01-04 21:41:33 +0800

    feat: 添加错误提示；添加ts；使用个人服务器生成sign；

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as querystring from "querystring";
 import * as md5 from "md5";
 import * as https from "https";
 import {appid, secret} from "../privacy";
+import * as http from "http";
+import * as buffer from "buffer";
 
 type BaiduResult = {
   error_code?: string;
@@ -14,17 +16,46 @@ type BaiduResult = {
   }[]
 }
 
-export const translate = (words) => {
-  const salt = Math.round(Math.random() * 100);
 
+const ERROR_MAP = {
+  52000: '成功',
+  52001: '请求超时',
+  52002: '系统错误',
+  52003: '未授权用户',
+  54000: '必填参数为空',
+  54001: '签名错误',
+  54003: '访问频率受限',
+  54004: '账户余额不足',
+  54005: '长query请求频繁',
+  58000: '客户端IP非法',
+  58001: '译文语言方向不支持',
+  58002: '服务当前已关闭',
+  90107: '认证未通过或未生效',
+};
+
+export const translate = async (words: string) => {
+  const salt = Math.round(Math.random() * 100);
+  let from, to;
+
+  if (/[a-zA-Z]/.test(words[0])) {
+    from = 'en';
+    to = 'zh';
+  } else {
+    from = 'zh';
+    to = 'en';
+  }
+
+  const res = await getSign<{ data: string }>(words, salt) as { data: string };
   const query = querystring.stringify({
     q: words,
-    from: 'en',
-    to: 'sdfsdfsd',
+    from,
+    to,
     appid,
     salt,
-    sign: md5(appid + words + salt + secret)
+    sign: res.data
   });
+
+  console.log('query', query);
 
   const options = {
     hostname: 'fanyi-api.baidu.com',
@@ -34,7 +65,7 @@ export const translate = (words) => {
   };
 
   const request = https.request(options, (response) => {
-    let chunks = [];
+    let chunks: Buffer[] = [];
     response.on('data', (chunk) => {
       chunks.push(chunk);
       // console.log(`响应主体: ${JSON.parse(chunk)}`);
@@ -43,7 +74,8 @@ export const translate = (words) => {
       const string = Buffer.concat(chunks).toString();
       const obj: BaiduResult = JSON.parse(string);
       if (obj.error_code) {
-        console.log('【错误】 错误码 ' + obj.error_code + ' ' + obj.error_msg);
+        // @ts-ignore
+        console.log('【错误】 ' + obj.error_code + ' ' + ERROR_MAP[obj.error_code] + ' ' + obj.error_msg);
         process.exit();
       } else {
         console.log('【翻译结果】', obj.trans_result.map(item => item.dst).join(','));
@@ -57,5 +89,33 @@ export const translate = (words) => {
 
 // 将数据写入请求主体。
   request.end();
-
 };
+
+function getSign<F>(q:string, salt: number) {
+  const query = querystring.stringify({
+    q,
+    salt,
+  });
+  const options = {
+    hostname: 'resume.glenni.ga',
+    path: '/apis?' + query,
+    method: 'GET',
+  };
+  return new Promise((resolve, reject) => {
+    const request = http.request(options, (response) => {
+      let chunks: Buffer[] = [];
+      response.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+      response.on('end', () => {
+        const string = Buffer.concat(chunks).toString();
+        const obj: F = JSON.parse(string);
+        resolve(obj);
+      });
+    });
+    request.on('error', (e) => {
+      reject(e);
+    });
+    request.end();
+  });
+}
